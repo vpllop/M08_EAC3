9cb79c3833f456dd150dcead5b5980ec284ebe68
valen <vpllop@hotmail.com>
2018-10-26 08:35:45 +0200

    Initial commit

---
+++ app/src/main/java/cat/valen/m08_eac3/MainActivity.java
@@ -0,0 +1,62 @@
+package cat.valen.m08_eac3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        //ft.replace(R.id.placeholder, new LlanternaFragment());
+        ft.add(R.id.placeholder, new LlanternaFragment());
+        // Complete the changes added above
+        ft.commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.menu_main, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_llanterna) {
+            // Begin the transaction
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            //ft.replace(R.id.placeholder, new LlanternaFragment());
+            ft.replace(R.id.placeholder, new LlanternaFragment());
+            // Complete the changes added above
+            ft.commit();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
+    }
+
+}
